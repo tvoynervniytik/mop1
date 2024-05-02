1b96dab311f849e19df719413bd174976645a1b6
202103 <202103@WINMAC-4>
2024-05-02 13:54:35 +0300

    Добавьте файлы проекта.

---
+++ mop/Functions/AuthorizationFunc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace mop.Functions
+{
+    internal class AuthorizationFunc
+    {
+        private static string login;
+        private static string password;
+
+        public void Authorization(string login, string password)
+        {
+            if (login == null || password == null) 
+            { 
+                if (login == null) 
+                {
+                    MessageBox.Show("Введите логин!", "login error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                if (password == null) 
+                {
+                    MessageBox.Show("Введите пароль!", "password error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
---
+++ mop/Pages/AuthorizationPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mop.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationPage.xaml
+    /// </summary>
+    public partial class AuthorizationPage : Page
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string login;
+            string password;
+            login = loginTb.Text.Trim();
+            password = passwordTb.Password.Trim();
+        }
+    }
+}
