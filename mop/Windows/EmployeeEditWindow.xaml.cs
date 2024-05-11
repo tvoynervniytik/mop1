2f61fca1a3351f53f09c05202aadff2d99938e76
TiMON <mavlik.delyus@mail.ru>
2024-05-11 03:02:32 +0300

    /

--- mop/Windows/EmployeeEditWindow.xaml.cs
+++ mop/Windows/EmployeeEditWindow.xaml.cs
@@ -25,15 +25,60 @@ namespace mop.Windows
         public static List<Posts> posts { get; set; }
         public static List<Education> educations { get; set; }
         private Employees empl = AuthorizationFunc.loggedUser;
+        private Requests req;
         public EmployeeEditWindow(Requests request)
         {
             InitializeComponent();
-            
+            attrTb.Text = request.AtributeName;
+            itemTb.Text = request.ItemEditted;
+            req = request;
             this.DataContext = this;
         }
 
         private void SaveBtn_Click(object sender, RoutedEventArgs e)
         {
+            Employees employee = AuthorizationFunc.loggedUser;
+            if (req.AtributeName == "имя") 
+            {
+                employee.Name = req.ItemEditted;
+                DBConnection.mop.SaveChanges();
+            }
+            if (req.AtributeName == "фамилия") 
+            {
+                employee.Surname = req.ItemEditted;
+                DBConnection.mop.SaveChanges();
+            }
+            if (req.AtributeName == "отчество") 
+            {
+                employee.Patronymic = req.ItemEditted;
+                DBConnection.mop.SaveChanges();
+            }
+            if (req.AtributeName == "почта") 
+            {
+                employee.Email = req.ItemEditted;
+                DBConnection.mop.SaveChanges();
+            }
+            if (req.AtributeName == "телефон") 
+            {
+                employee.Phone = req.ItemEditted;
+                DBConnection.mop.SaveChanges();
+            }
+            if (req.AtributeName == "логин") 
+            {
+                employee.Login = req.ItemEditted;
+                DBConnection.mop.SaveChanges();
+            }
+            if (req.AtributeName == "пароль")
+            {
+                employee.Password = req.ItemEditted;
+                DBConnection.mop.SaveChanges();
+            }
+
+            if (req.AtributeName == "паспорт") 
+            {
+                employee.Passport = req.ItemEditted;
+                DBConnection.mop.SaveChanges();
+            }
             this.Close();
             MessageBox.Show($"Сотрудник {empl.Surname} {empl.Surname.First()}. {empl.Patronymic.First()}. успешно изменён");
             
